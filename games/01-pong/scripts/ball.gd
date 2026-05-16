f116b9e10f9e08b6b2c350efd2edca1966f773a0
zilhak <zilhak3@naver.com>
2026-05-16 16:20:27 +0900

    feat(pong): 공-패들 충돌 반사 구현
    - Rect2.intersects()로 패들과 충돌 검사
    - 패들 중앙으로부터의 오프셋에 따라 반사 각도 가변 (-43°~+43°)

--- games/01-pong/scripts/ball.gd
+++ games/01-pong/scripts/ball.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var speed: float = 420.0
 
 const BALL_SIZE: float = 20.0
+const PADDLE_WIDTH: float = 20.0
+const PADDLE_HEIGHT: float = 100.0
+const MAX_BOUNCE_ANGLE: float = 0.75
 const VIEWPORT_WIDTH: float = 1152.0
 const VIEWPORT_HEIGHT: float = 648.0
 const START_POSITION := Vector2(
@@ -11,6 +14,9 @@ const START_POSITION := Vector2(
 )
 
 var velocity: Vector2 = Vector2.ZERO
+
+@onready var left_paddle: Node2D = get_node("../LeftPaddle")
+@onready var right_paddle: Node2D = get_node("../RightPaddle")
 
 func _ready() -> void:
 	reset_and_launch()
@@ -24,6 +30,26 @@ func reset_and_launch() -> void:
 func _process(delta: float) -> void:
 	position += velocity * delta
 	_bounce_off_walls()
+	_bounce_off_paddles()
+
+func _bounce_off_paddles() -> void:
+	var ball_rect := Rect2(position, Vector2(BALL_SIZE, BALL_SIZE))
+	var left_rect := Rect2(left_paddle.position, Vector2(PADDLE_WIDTH, PADDLE_HEIGHT))
+	var right_rect := Rect2(right_paddle.position, Vector2(PADDLE_WIDTH, PADDLE_HEIGHT))
+
+	if velocity.x < 0.0 and ball_rect.intersects(left_rect):
+		position.x = left_paddle.position.x + PADDLE_WIDTH
+		_apply_paddle_bounce(left_paddle.position.y, 1.0)
+	elif velocity.x > 0.0 and ball_rect.intersects(right_rect):
+		position.x = right_paddle.position.x - BALL_SIZE
+		_apply_paddle_bounce(right_paddle.position.y, -1.0)
+
+func _apply_paddle_bounce(paddle_y: float, dir_x: float) -> void:
+	var ball_center_y: float = position.y + BALL_SIZE / 2.0
+	var paddle_center_y: float = paddle_y + PADDLE_HEIGHT / 2.0
+	var offset: float = clampf((ball_center_y - paddle_center_y) / (PADDLE_HEIGHT / 2.0), -1.0, 1.0)
+	var angle: float = offset * MAX_BOUNCE_ANGLE
+	velocity = Vector2(cos(angle) * dir_x, sin(angle)) * speed
 
 func _bounce_off_walls() -> void:
 	if position.y <= 0.0 and velocity.y < 0.0:
